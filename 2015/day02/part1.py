a5bc3d29a5f675fdd610b38e3397021186e8a715
atrox3d <atrox3d@gmail.com>
2024-01-30 19:22:21 +0100

    tests and errors

--- 2015/day02/part1.py
+++ 2015/day02/part1.py
@@ -1,10 +1,46 @@
 from pathlib import Path
+import sys
 
-with open(Path(__file__).with_suffix('.txt')) as fp:
-    input_text = fp.read()
+def get_total_paper(l, w, h):
+    return 2*l*w + 2*w*h + 2*h*l + min(l, w, h)**2
 
-up = input_text.count('(')
-down = input_text.count(')')
+def convert_box(box):
+    return tuple(map(int, box))
 
-print(f'{up, down = }')
-print(f'{up - down = }')
+def parse_box(line):
+    return line.rstrip().split('x')
+
+def compute_paper(box_str):
+    print(f'{box_str = }')
+    box = parse_box(box_str)
+    print(f'{box = }')
+    int_box = convert_box(box)
+    print(f'{int_box = }')
+    total = get_total_paper(*int_box)
+    print(f'{total = }')
+    return total
+
+
+if sys.argv[1:]:
+    param = sys.argv[1]
+    if param.lower() == 'test':
+        tests = {'2x3x4':58, '1x1x10':43}
+        for test, expected in tests.items():
+            print(f'testing {test=}: {expected=}')
+            result = compute_paper(test)
+            assert expected == result, f'{expected=} != {result=}'
+    elif param.count('x') == 2:
+        compute_paper(param)
+    else:
+        raise ValueError(f'wrong parameter format {param}')
+else:
+    with open(Path(__file__).with_suffix('.txt')) as fp:
+        lines = [line.rstrip() for line in fp.readlines()]
+        boxes = [parse_box(line) for line in lines]
+        int_boxes = [convert_box(box) for box in boxes]
+        papers = [get_total_paper(l, w, h) for l, w, h in int_boxes]
+        for line, box, int_box, paper in zip(lines, boxes, int_boxes, papers):
+            print(f'{line, box, int_box,  paper = }')
+        total = sum(papers)
+        print(total)
+
