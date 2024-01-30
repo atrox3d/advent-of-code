c9f24eb6e86ecc673e4c0e656dc10d12e03d4a40
atrox3d <atrox3d@gmail.com>
2024-01-30 14:58:35 +0100

    solved day 1 part 1

--- 2015/day01/main.py
+++ 2015/day01/main.py
@@ -1,4 +1,10 @@
 from pathlib import Path
 
 with open(Path(__file__).parent / 'input.txt') as fp:
-    print(fp.read())
+    input_text = fp.read()
+
+up = input_text.count('(')
+down = input_text.count(')')
+
+print(f'{up, down = }')
+print(f'{up - down = }')
