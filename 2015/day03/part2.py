6ff0f8ce913fbfb93f0fb4555e8eb303444fda0a
atrox3d <atrox3d@gmail.com>
2024-01-31 05:15:19 +0100

    solved day 3 part 2

--- 2015/day03/part2.py
+++ 2015/day03/part2.py
@@ -1,31 +1,41 @@
 from pathlib import Path
 import sys
-            
+
+def get_pos(step, r, c):
+    if step == '>':
+        c += 1
+    elif step == '<':
+        c -= 1
+    elif step == 'v':
+        r += 1
+    elif step == '^':
+        r -= 1
+    return r, c
+
 def solution(quiz_input):
     r, c = 0, 0
     start = r, c
     path = set()
     path.add(start)
-    for step in quiz_input:
-        if step == '>':
-            c += 1
-        elif step == '<':
-            c -= 1
-        elif step == 'v':
-            r += 1
-        elif step == '^':
-            r -= 1
-        tile = r, c
-        path.add(tile)
+    for step in quiz_input[::2]:
+        r, c = santatile = get_pos(step, r, c)
+        path.add(santatile)
+
+    r, c = 0, 0
+    start = r, c
+    for robostep in quiz_input[1::2]:
+        r, c = robotile = get_pos(robostep, r, c)
+        path.add(robotile)
+
     return len(path)
 
 if sys.argv[1:]:
     param = sys.argv[1]
     if param.lower() == 'test':
         tests = {
-            '>': 2,
-            '^>v<': 4,
-            '^v^v^v^v^v': 2,
+            '^v': 3,
+            '^>v<': 3,
+            '^v^v^v^v^v': 11,
         }
         for test, expected in tests.items():
             print(f'testing {test=}: {expected=}')
