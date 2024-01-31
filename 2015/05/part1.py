58962fc7fb5260835fd52eef9b0a135b3abfc6e9
atrox3d <atrox3d@gmail.com>
2024-01-31 13:10:43 +0100

    has_three_vowels

--- 2015/05/part1.py
+++ 2015/05/part1.py
@@ -8,11 +8,13 @@ def solution(quiz_input):
 
     A nice string is one with all of the following properties:
 
-    It contains at least three vowels (aeiou only), like aei, 
+    - It contains at least three vowels (aeiou only), like aei, 
     xazegov, or aeiouaeiouaeiou.
-    It contains at least one letter that appears twice in a row, 
+
+    - It contains at least one letter that appears twice in a row, 
     like xx, abcdde (dd), or aabbccdd (aa, bb, cc, or dd).
-    It does not contain the strings ab, cd, pq, or xy, even 
+
+    - It does not contain the strings ab, cd, pq, or xy, even 
     if they are part of one of the other requirements.
     
     For example:
@@ -30,18 +32,34 @@ def solution(quiz_input):
     haegwjzuvuyypxyu is naughty because it contains the string xy.
 
     dvszwmarrgswjxmb is naughty because it contains only one vowel.
-    
+
     How many strings are nice?
     """
-    return quiz_input
+    VOWELS = 'aeiou'
+    FORBIDDEN = 'ab', 'cd', 'pq', 'xy'
+    
+    def has_three_vowels(word: str) -> bool:
+        return len([v for v in word if v in VOWELS])
+
+    for word in quiz_input:
+        print(f'{word} -> {has_three_vowels(word)}')
+    # return dict(nice=0, naughty=0)
 
 if sys.argv[1:]:
     param = sys.argv[1]
     if param.lower() == 'test':
         tests = [
             {
-                'input': '', 
-                'expected': 0,
+                'input': ['aaa'], 
+                'expected': {'nice': 1, 'naughty': 0},
+            },
+            {
+                'input': ['jchzalrnumimnmhp'], 
+                'expected': {'nice': 0, 'naughty': 1},
+            },
+            {
+                'input': ['dvszwmarrgswjxmb'], 
+                'expected': {'nice': 0, 'naughty': 1},
             },
         ]
         for test in tests:
@@ -58,5 +76,5 @@ if sys.argv[1:]:
                 print()
 else:
     with open(Path(__file__).with_suffix('.txt')) as fp:
-        quiz_input = fp.read().rstrip()
+        quiz_input = [line.rstrip() for line in fp.readlines()]
         print(solution(quiz_input))
