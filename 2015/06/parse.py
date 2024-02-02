cb6dd801e48aad378e25a6a2b9ea670bc3a27ec7
atrox3d <atrox3d@gmail.com>
2024-02-02 07:31:55 +0100

    parse.py

---
+++ 2015/06/parse.py
@@ -0,0 +1,40 @@
+import argparse
+import sys
+
+import helpers
+
+
+def get_parser() -> argparse.ArgumentParser:
+    # create parser
+    parser = argparse.ArgumentParser(
+        description="aoc parser"
+    )
+
+    # parser.add_argument('positional1')
+
+    # parser.add_argument(
+    #     # '-o'
+    #     '-offset',
+    #     '--offset',
+    #     metavar='',
+    #     type=str,
+    #     help='offset',
+    #     required=True
+    # )
+
+    parser.add_argument('test_val', nargs='?')
+    parser.add_argument('-p', '--print', action='store_true')
+    parser.add_argument('-t', '--test',  action='store_true')
+
+    # parser.add_argument('file', type=argparse.FileType('r'))
+    # args = parser.parse_args()
+    # print(args.file.readlines())
+    # parser.add_argument('-i', '--input',
+    #                     type=argparse.FileType('r', encoding='utf-8'), 
+    #                     required=False,
+    #                     default=str(helpers.INPUT_PATH)
+    #                     )
+    parser.add_argument('-i', '--input',
+                        default=helpers.INPUT_PATH
+                        )
+    return parser
