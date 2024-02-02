d2fbf99ffc761d1a5a5ea0426a0e20698678bb39
atrox3d <atrox3d@gmail.com>
2024-02-02 06:28:12 +0100

    logger format

--- 2015/06/helpers.py
+++ 2015/06/helpers.py
@@ -9,21 +9,21 @@ INPUT_PATH = Path(__file__).parent / INPUT_FILE
 def is_multiline(input_path=INPUT_PATH):
     with open(input_path) as fp:
         multiline = '\n' in fp.read()
-    logger.debug(f'helpers.is_multiline: inputs is {"multiline" if multiline else "just data"}')
+    logger.debug(f'inputs is {"multiline" if multiline else "just data"}')
     return multiline
 
 def get_input(input_path=INPUT_PATH):
     with open(input_path) as fp:
         data = fp.read()
         if '\n' in data:
-            logger.debug(f'helpers.get_input: parsing input lines')
+            logger.debug(f'parsing input lines')
             data = [line.rstrip() for line in data]
         else:
-            logger.debug(f'helpers.get_input: reading input data')
+            logger.debug(f'reading input data')
     return data
 
 def print_input(data=None, input_path=INPUT_PATH):
-    logger.debug(f'helpers.print_input: printing data')
+    logger.debug(f'printing data')
     data = data or get_input(input_path)
     if is_multiline(input_path):
         for line in data:
--- 2015/06/main.py
+++ 2015/06/main.py
@@ -4,22 +4,25 @@ import logging
 import helpers
 import tests
 
-logging.basicConfig(level='DEBUG')
+logging.basicConfig(level='DEBUG', format='%(levelname)5.5s|%(module)12.12s|%(funcName)15.15s| %(message)s')
 logger = logging.getLogger(__name__)
 
 def solution(quiz_input):
     """
     """
-    helpers.print_input(quiz_input)
     return False
 
 if __name__ == '__main__':
     if sys.argv[1:]:
         param = sys.argv[1]
-        logger.info(f'param detected:')
+        logger.info(f'param detected: {param!r}')
         if param.lower() == 'test':
             logger.info(f'testing solution against tests')
             tests.test(solution)
+        elif param.lower() == 'print':
+            quiz_input = helpers.get_input()
+            logger.info(f'printing input')
+            helpers.print_input(quiz_input)
         else:
             logger.info(f'testing solution against {param}')
             tests.test_param(solution, param)
