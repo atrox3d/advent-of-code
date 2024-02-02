07b8489f4f041bd0ed9070dfbc012ca8fcc84d2f
atrox3d <atrox3d@gmail.com>
2024-02-02 08:49:00 +0100

    add mutex options

--- 2015/06/parse.py
+++ 2015/06/parse.py
@@ -7,28 +7,11 @@ def get_parser() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(
         description="aoc parser"
     )
-
-    # parser.add_argument('positional1')
-
-    # parser.add_argument(
-    #     # '-o'
-    #     '-offset',
-    #     '--offset',
-    #     metavar='',
-    #     type=str,
-    #     help='offset',
-    #     required=True
-    # )
-    # parser.add_argument('file', type=argparse.FileType('r'))
-    # args = parser.parse_args()
-    # print(args.file.readlines())
-    # parser.add_argument('-i', '--input',
-    #                     type=argparse.FileType('r', encoding='utf-8'), 
-    #                     required=False,
-    #                     default=str(helpers.INPUT_PATH)
-    #                     )
     parser.add_argument('test_val', nargs='?')
-    parser.add_argument('-p', '--print', action='store_true')
-    parser.add_argument('-t', '--test',  action='store_true')
+    
+    mutex = parser.add_mutually_exclusive_group()
+    mutex.add_argument('-p', '--print', action='store_true')
+    mutex.add_argument('-t', '--test',  action='store_true')
+    
     parser.add_argument('-i', '--input_path', default=helpers.INPUT_PATH)
     return parser
