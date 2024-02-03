977ca6896a20effc1d81fbb1af5d8ec65d489b54
atrox3d <atrox3d@gmail.com>
2024-02-03 06:37:48 +0100

    setup 2015 7

---
+++ 2015/07/solution.py
@@ -0,0 +1,13 @@
+import logging
+
+import main
+
+logger = logging.getLogger(__name__)
+
+def solution(quiz_input):
+    """
+    """
+    return None
+
+if __name__ == '__main__':
+    main.main(solution, level='INFO')
