f900292c5bf3525b300a31f5978d786cb4026b53
atrox3d <atrox3d@gmail.com>
2024-02-07 19:34:05 +0100

    update template

--- template/solution.py
+++ template/solution.py
@@ -1,15 +1,24 @@
+"""
+"""
 import logging
 import sys, os
+import re, json
+from pathlib import Path
 
 sys.path.append(os.getcwd())
 from aoclib import main
 
 logger = logging.getLogger(__name__)
-
 def solution(quiz_input):
-    """
-    """
-    return None
+    pass
 
 if __name__ == '__main__':
-    main.main(solution, level='INFO')
+    LOGFILE = str(Path(sys.argv[0]).parent / Path(__file__).stem) + '.log'
+    handlers = [
+        logging.FileHandler(
+            LOGFILE,
+            mode='w'
+            ),
+        logging.StreamHandler()
+    ]
+    main.main(solution, level='DEBUG', handlers=handlers)
