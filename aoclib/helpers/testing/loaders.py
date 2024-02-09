581474be4dbf558afe47220036cf3019642fbf66
atrox3d <atrox3d@gmail.com>
2024-02-09 13:39:41 +0100

    move loader functions outside get_loader

--- aoclib/helpers/testing/loaders.py
+++ aoclib/helpers/testing/loaders.py
@@ -7,37 +7,38 @@ import csv
 import json
 from pathlib import Path
 
+def json_loader(input_path):
+    with open(input_path) as fp:
+        data = json.load(fp)
+        logger.debug(f'{data = }')
+        for d in data:
+            for k, v in d.items():
+                if '\n' in v:
+                    d[k] = [line for line in v.split('\n') if line]
+        logger.debug(f'{data = }')
+        data = [tuple(item.values()) for item in data]
+        logger.debug(f'{data = }')
+    return data
+
+def str_to_val(value):
+    if value.isnumeric():
+        value = int(value)
+    elif value.lower() in ('true', 'false'):
+        value = bool(value)
+    elif value.lower() == 'none':
+        value = None
+    return value
+
+def csv_loader(input_path):
+    with open(input_path) as fp:
+        reader = csv.reader(fp,)
+        lines = [list(map(str.strip, line)) for line in reader]
+        logger.debug(f'{lines = }')
+        convert = [[str_to_val(value) for value in values] for values in lines]
+        return convert
+
 
 def get_loader(input_path):
-    def json_loader(input_path):
-        with open(input_path) as fp:
-            data = json.load(fp)
-            logger.debug(f'{data = }')
-            for d in data:
-                for k, v in d.items():
-                    if '\n' in v:
-                        d[k] = [line for line in v.split('\n') if line]
-            logger.debug(f'{data = }')
-            data = [tuple(item.values()) for item in data]
-            logger.debug(f'{data = }')
-        return data
-
-    def str_to_val(value):
-        if value.isnumeric():
-            value = int(value)
-        elif value.lower() in ('true', 'false'):
-            value = bool(value)
-        elif value.lower() == 'none':
-            value = None
-        return value
-
-    def csv_loader(input_path):
-        with open(input_path) as fp:
-            reader = csv.reader(fp,)
-            lines = [list(map(str.strip, line)) for line in reader]
-            logger.debug(f'{lines = }')
-            convert = [[str_to_val(value) for value in values] for values in lines]
-            return convert
 
     suffix = Path(input_path).suffix
     if suffix == '.json':
