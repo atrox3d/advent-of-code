42f8a6cfa5c9a610e0a5d1c84db72145c79e5cac
atrox3d <atrox3d@gmail.com>
2024-02-14 13:57:03 +0100

    update solution

--- 2015/11/solution.py
+++ 2015/11/solution.py
@@ -1,6 +1,6 @@
 """
-https://adventofcode.com/2015/day/11
-https://adventofcode.com/2015/day/11#part2
+https://adventofcode.com/YEAR/day/DAY
+https://adventofcode.com/YEAR/day/DAY#part2
 """
 import logging
 import sys, os
@@ -12,7 +12,89 @@ from aoclib import main
 
 logger = logging.getLogger(__name__)
 def solution(quiz_input):
-    pass
+    '''
+    --- Day 11: Corporate Policy ---
+    Santa's previous password expired, and he needs help choosing 
+    a new one.
+
+    To help him remember his new password after the old one expires, 
+    Santa has devised a method of coming up with a password based on 
+    the previous one. 
+    Corporate policy dictates that passwords must be exactly 
+    eight lowercase letters (for security reasons), 
+    so he finds his new password by incrementing his old 
+    password string repeatedly until it is valid.
+
+    Incrementing is just like counting with numbers: 
+    xx, xy, xz, ya, yb, and so on. 
+    Increase the rightmost letter one step; 
+    if it was z, it wraps around to a, and repeat with the 
+    next letter to the left until one doesn't wrap around.
+
+    Unfortunately for Santa, a new Security-Elf recently started, 
+    and he has imposed some additional password requirements:
+
+    Passwords must include one increasing straight of at least 
+    three letters, like abc, bcd, cde, and so on, up to xyz. 
+    They cannot skip letters; abd doesn't count.
+    
+    Passwords may not contain the letters i, o, or l, 
+    as these letters can be mistaken for other characters and 
+    are therefore confusing.
+    
+    Passwords must contain at least two different, 
+    non-overlapping pairs of letters, like aa, bb, or zz.
+    For example:
+
+    hijklmmn meets the first requirement (because it contains the 
+    straight hij) but fails the second requirement requirement 
+    (because it contains i and l).
+    
+    abbceffg meets the third requirement (because it repeats bb and ff) 
+    but fails the first requirement.
+    
+    abbcegjk fails the third requirement, because it only has one 
+    double letter (bb).
+    
+    The next password after abcdefgh is abcdffaa.
+
+    The next password after ghijklmn is ghjaabcc, 
+    because you eventually skip all the passwords that start with 
+    ghi..., since i is not allowed.
+    
+    Given Santa's current password (your puzzle input), 
+    what should his next password be?
+
+    Your puzzle input is cqjxjnds.    
+    '''
+    def increment(password: str) -> str:
+        '''
+        Incrementing is just like counting with numbers: 
+        xx, xy, xz, ya, yb, and so on. 
+        Increase the rightmost letter one step; 
+        if it was z, it wraps around to a, and repeat with the 
+        next letter to the left until one doesn't wrap around.
+        '''
+        from string import ascii_lowercase
+        print(password)
+        # print(password[::-1])
+        newpassword = ''
+        for pos, char in enumerate(password[::-1]):
+            find = ascii_lowercase.index(char)
+            try:
+                new_char = ascii_lowercase[find+1]
+                newpassword += new_char
+                newpassword += password[::-1][pos+1:]
+                break
+            except IndexError:
+                new_char = ascii_lowercase[0]
+                newpassword += new_char
+        
+        print (newpassword[::-1])
+        return newpassword[::-1]
+                
+    result = increment(quiz_input)
+    return result
 
 if __name__ == '__main__':
     LOGFILE = str(Path(sys.argv[0]).parent / Path(__file__).stem) + '.log'
@@ -20,4 +102,11 @@ if __name__ == '__main__':
         logging.FileHandler(LOGFILE, mode='w'),
         logging.StreamHandler()
     ]
-    main.main(solution, input=None, level='DEBUG', handlers=handlers)
+    main.main(
+                solution, 
+                input=None,
+                test_input='ghijklmnz',
+                test_expected='ghjaabcc', 
+                level='DEBUG', 
+                handlers=handlers
+            )
