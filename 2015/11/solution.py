88835cbedeff393bccbf324ec16b48c61c34ef74
atrox3d <atrox3d@gmail.com>
2024-02-15 09:03:50 +0100

    rincrement

--- 2015/11/solution.py
+++ 2015/11/solution.py
@@ -92,8 +92,31 @@ def solution(quiz_input):
         
         print (newpassword[::-1])
         return newpassword[::-1]
+    
+    def rincrement(password: str) -> str:
+        '''
+        Incrementing is just like counting with numbers: 
+        xx, xy, xz, ya, yb, and so on. 
+        Increase the rightmost letter one step; 
+        if it was z, it wraps around to a, and repeat with the 
+        next letter to the left until one doesn't wrap around.
+        '''
+        from string import ascii_lowercase
+
+        if password == '':
+            print()
+            return ''
+        print(f'{password = }')
+        print(f'{password[0] = }')
+        print(f'{password[1:] = }')
+        ret = rincrement(password[1:])
+        print(f'{ret = }, {password[0] = }')
+
+        if ret == '':
+            pass
+        return  ret + password[0]
                 
-    result = increment(quiz_input)
+    result = rincrement(quiz_input)
     return result
 
 if __name__ == '__main__':
