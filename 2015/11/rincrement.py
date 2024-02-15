27eeceefda8608a4a8b99f69b78cf87740231cc6
atrox3d <atrox3d@gmail.com>
2024-02-15 10:36:09 +0100

    preparing

--- 2015/11/rincrement.py
+++ 2015/11/rincrement.py
@@ -1,6 +1,14 @@
 from string import ascii_lowercase
 
-def rincrement(password: str, valid_chars) -> str:
+
+def rincrement(password: str) -> str:
+    forbidden = 'iol'
+    valid = ''.join(char for char in ascii_lowercase if char not in forbidden)
+
+    incremented = _rincrement(password[::-1], valid)[::-1]
+    
+
+def _rincrement(password: str, valid_chars) -> str:
     '''
     Incrementing is just like counting with numbers: 
     xx, xy, xz, ya, yb, and so on. 
@@ -8,29 +16,16 @@ def rincrement(password: str, valid_chars) -> str:
     if it was z, it wraps around to a, and repeat with the 
     next letter to the left until one doesn't wrap around.
     '''
-    print(f'{password = !r}')
-
     if password == '':
         return ''
-
     char = newchar = password[0]
-    print(f'{char = }')
     partition = valid_chars.partition(char)[-1]
-    print(f'{partition = }')
     if partition:
         newchar = partition[0]
-        ret = newchar + password[1:]
-        print(ret)
-        return ret
+        return newchar + password[1:]
     else:
         newchar = valid_chars[0] # a
-        ret = newchar + rincrement(password[1:], valid_chars)
-        print(ret)
-        return ret
-    # print(newchar)
+        return newchar + _rincrement(password[1:], valid_chars)
 
-# rincrement('zzz')
-valid = ''.join(char for char in ascii_lowercase if char not in 'iol')
-print(f'{valid = }')
-result = rincrement('za'[::-1], valid)[::-1]
+result = rincrement('aaz')
 print(f'{result = }')
