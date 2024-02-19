bb908ed71e13debdf5c60de30d5300723c20cef1
atrox3d <atrox3d@gmail.com>
2024-02-19 11:35:28 +0100

    gencombos

--- 2015/15/mixer.py
+++ 2015/15/mixer.py
@@ -30,18 +30,3 @@ def bestmixes(target, portions, max, level=None, cache=None):
                 except:
                     pass
     return combo
-
-def gencombos( target, column ):
-    if column == 1:
-        yield [target]
-    else:
-        print(f'for i in range( 0, target//column+1 ):')
-        print(f'for i in range( 0, {target//column+1} ):')
-        for i in range( 0, target//column+1 ):
-            print(f'for row in gencombos( target-i*column, column-1 ):')
-            print(f'for row in gencombos( {target-i*column}, {column-1} ):')
-            for row in gencombos( target-i*column, column-1 ):
-                yield row+[i]
-
-for row in gencombos( 7, 3 ):
-    print(row)
