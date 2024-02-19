2dc76078e811c279a9e465b665b8a47e4c761f4f
atrox3d <atrox3d@gmail.com>
2024-02-19 12:36:28 +0100

    working-version

--- gencombos.py
+++ gencombos.py
@@ -3,11 +3,11 @@ def gencombos( target, column ):
     if column == 1:
         yield [target]
     else:
-        print(f'for i in range( 0, target//column+1 ):')
-        print(f'for i in range( 0, {target//column+1} ):')
+        # print(f'for i in range( 0, target//column+1 ):')
+        # print(f'for i in range( 0, {target//column+1} ):')
         for i in range( 0, target//column+1 ):
-            print(f'for row in gencombos( target-i*column, column-1 ):')
-            print(f'for row in gencombos( {target-i*column}, {column-1} ):')
+            # print(f'for row in gencombos( target-i*column, column-1 ):')
+            # print(f'for row in gencombos( {target-i*column}, {column-1} ):')
             for row in gencombos( target-i*column, column-1 ):
                 yield row+[i]
 
