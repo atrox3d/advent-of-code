31fdf52c8b7d8c43e85ab92743a49a24407f4557
atrox3d <atrox3d@gmail.com>
2024-02-21 11:16:33 +0100

    debug

--- 2015/15/balancesum.py
+++ 2015/15/balancesum.py
@@ -8,40 +8,6 @@
     0 1 2
     0 0 3
 '''
-def subrange (n):
-    if n == 0:              # base case
-        return [0]
-    
-    ret = subrange(n-1)     # towards base case
-    # reverse
-    #      [0]      1
-    #      [0,1]    2
-    #      [0,1,2]  3
-    return ret   + [n]      # or return [*ret] + [n]
-    
-    # normal
-    #       1    [0]
-    #       2    [1,0]
-    #       3    [2,1,0]
-    return [n] + ret        # or return [n] + [*ret]
-
-def add_range (n, start=0):
-    if start == n:
-        return []                   # base case
-    
-    ret = add_range(n, start+1)     # towards base case
-    print(ret)  # [0], [1, 0], [2, 1, 0], ...
-    # normal
-    #         2      []
-    #         1      [2]
-    #         0      [1,2]
-    return [start] + ret    # or [start] + [*ret]
-    # reverse
-    #      []     2
-    #      [2]    1
-    #      [2,1]  0
-    return ret + [start]    # or [*ret] + [start]
-
 verbose = True
 def vprint(*args, **kwargs):
     if verbose:
