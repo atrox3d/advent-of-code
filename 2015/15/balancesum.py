01aad83015cc0a5dfd895f069000c62009836081
atrox3d <atrox3d@gmail.com>
2024-02-22 06:06:49 +0100

    remove vprint

--- 2015/15/balancesum.py
+++ 2015/15/balancesum.py
@@ -1,18 +1,3 @@
-'''
-3   0 0 0
-    3 0 0
-    2 1 0
-    1 2 0
-    0 3 0
-    0 2 1
-    0 1 2
-    0 0 3
-'''
-verbose = False
-def vprint(*args, **kwargs):
-    if verbose:
-        print(*args, **kwargs)
-
 def rng(start, end, direction=None):
     # detect direction on the first call
     if direction is None:
@@ -26,26 +11,20 @@ def rng(start, end, direction=None):
             # non inclusive of last element
             end -= 1
 
-    vprint(f'{start, end, direction = }')
     # base case
     if end == start:
         yield end
     else:
         # recursive case, ret is a generator
         ret = rng(start, end -1, direction)
-        vprint(f'{ret = }')
         # yield elements in the right order
         if direction > 0:
             for val in ret:
-                vprint(f'{val = }')
-                vprint(f'yield {val=}')
                 yield val
             yield end
         elif direction < 0:
             yield end
             for val in ret:
-                vprint(f'{val = }')
-                vprint(f'yield {val=}')
                 yield val
 
 if __name__ == '__main__':
