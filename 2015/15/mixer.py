9ec73de075d3a498415263556ce5014a504e12f7
atrox3d <atrox3d@gmail.com>
2024-02-24 16:15:58 +0100

    retry

--- 2015/15/mixer.py
+++ 2015/15/mixer.py
@@ -1,34 +1,11 @@
-def bestmixes(target, portions, max, level=None, cache=None):
-    print(f'{target = }')
-    cache = {} if cache is None else cache
-    level = 0 if level is None else level
-    level += 1
-    if level > max: return None
-    if target < 0: return None
-    if target == 0: return []
-
-    try:
-        return cache[target]
-    except:
-        pass
+def bestmixes(spoons, portions):
+    recipe = []
+    for spoon in range(spoons+1):
+        # for portion in range(portions):
+        recipe.append((spoons-spoon, spoon))
     
-    print(f'{level = }')
+    return recipe
 
-    combo = []
-    for portion in portions:
-        print(f'{portion = }')
-        retcombos = bestmixes(target - portion, portions, max, level)
-        print(f'{retcombos = }')
-        if retcombos is not None:
-            # for combo in retcombos:
-            if combo is not None:
-                combo = [*combo, portion]
-                # combos.append(combo)
-                try:
-                    cache[target] = combo
-                    print(cache)
-                except:
-                    pass
-    return combo
+recipe = bestmixes(5, 2)
 
-bestmixes(10, 4, 4)
+print(recipe)
