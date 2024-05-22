8a30eeecf5584b3404ecef33d24def7a43fa8757
atrox3d <atrox3d@gmail.com>
2024-05-22 07:41:30 +0200

    add create_json_tests

--- setup.py
+++ setup.py
@@ -1,7 +1,14 @@
 from pathlib import Path
+import json
+
+def create_json_tests(target_path:Path, filename='tests.json'):
+    data = [{"input": None, "expected": None}]
+    with open(str(target_path / filename), 'w') as fp:
+        json.dump(fp, data)
 
 
 def setup(target_path:Path|str):
+    target_path = Path(target_path)
     print(f'setting up {target_path!s}')
     if target_path.exists():
         raise FileExistsError(f'target path exists: {target_path}')
