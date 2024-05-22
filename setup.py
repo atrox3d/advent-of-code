0175d113faf892a155316c62f405d5c33ef0d29c
atrox3d <atrox3d@gmail.com>
2024-05-22 07:48:03 +0200

    add create_csv_tests

--- setup.py
+++ setup.py
@@ -1,10 +1,19 @@
 from pathlib import Path
 import json
+import csv
 
 def create_json_tests(target_path:Path, filename='tests.json'):
     data = [{"input": None, "expected": None}]
     with open(str(target_path / filename), 'w') as fp:
         json.dump(fp, data)
+
+def create_csv_tests(target_path:Path, filename='tests.csv'):
+    data = [{"input": None, "expected": None}]
+    with open(str(target_path / filename), 'w') as fp:
+        writer = csv.DictWriter(fp, data[0].keys())
+        writer.writeheader()
+        for row in data:
+            writer.writerow(row)
 
 
 def setup(target_path:Path|str):
