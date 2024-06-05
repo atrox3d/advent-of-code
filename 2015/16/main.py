503dd868cb4f1ed1834041565d583b20a0f1331a
atrox3d <atrox3d@gmail.com>
2024-06-05 14:53:50 +0200

    prepare data

--- 2015/16/main.py
+++ 2015/16/main.py
@@ -9,7 +9,10 @@ You have 500 Aunts named "Sue".
 
 So, to avoid sending the card to the wrong person, you need to figure out which Aunt Sue (which you conveniently number 1 to 500, for sanity) gave you the gift. You open the present and, as luck would have it, good ol' Aunt Sue got you a My First Crime Scene Analysis Machine! Just what you wanted. Or needed, as the case may be.
 
-The My First Crime Scene Analysis Machine (MFCSAM for short) can detect a few specific compounds in a given sample, as well as how many distinct kinds of those compounds there are. According to the instructions, these are what the MFCSAM can detect:
+The My First Crime Scene Analysis Machine (MFCSAM for short) can detect a 
+few specific compounds in a given sample, 
+as well as how many distinct kinds of those compounds there are. 
+According to the instructions, these are what the MFCSAM can detect:
 
 children, by human DNA age analysis.
 cats. It doesn't differentiate individual breeds.
@@ -18,7 +21,10 @@ goldfish. No other kinds of fish.
 trees, all in one group.
 cars, presumably by exhaust or gasoline or something.
 perfumes, which is handy, since many of your Aunts Sue wear a few kinds.
-In fact, many of your Aunts Sue have many of these. You put the wrapping from the gift into the MFCSAM. It beeps inquisitively at you a few times and then prints out a message on ticker tape:
+In fact, many of your Aunts Sue have many of these. 
+You put the wrapping from the gift into the MFCSAM. 
+It beeps inquisitively at you a few times and then prints out a message 
+on ticker tape:
 
 children: 3
 cats: 7
@@ -38,15 +44,44 @@ What is the number of the Sue that got you the gift?
 
 from pathlib import Path
 import logging
-
+import re
 logger = logging.getLogger(__name__)
 
+def parse_aunts(quiz_input:str) -> dict:
+    aunts = {}
+    aunt_pattern = r'^Sue (?P<id>\d+): (?P<properties>.+)'
+    props_pattern = r'(?P<thing>\w+): (?P<qty>\d+)'
+    for line in quiz_input.splitlines():
+        result = re.match(aunt_pattern, line)
+        aunt_id, props = result.groupdict().values()
+        aunts[aunt_id] = {}
+        for prop in props.split(', '):
+            thing, qty = re.match(props_pattern, prop).groupdict().values()
+            aunts[aunt_id].update({thing:int(qty)})
+    return aunts
+
+def get_tape() -> dict:
+    return {
+        'children': 3,
+        'cats': 7,
+        'samoyeds': 2,
+        'pomeranians': 3,
+        'akitas': 0,
+        'vizslas': 0,
+        'goldfish': 5,
+        'trees': 3,
+        'cars': 2,
+        'perfumes': 1,
+    }
+
+
 def solution1(quiz_input):
-    print(f'{quiz_input = !r}')
-    return None
+    aunts = parse_aunts(quiz_input)
+    for auntid, things in aunts.items():
+        print(auntid, things)
 
 def solution2(quiz_input):
-    print(f'{quiz_input = !r}')
+    # print(f'{quiz_input = !r}')
     return None
 
 def load_input(filename):
