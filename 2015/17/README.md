1281c94c43c178ae5c7c74d655ffb706c1f90cff
atrox3d <atrox3d@gmail.com>
2024-06-07 07:32:03 +0200

    2015/17/1

---
+++ 2015/17/README.md
@@ -0,0 +1,4 @@
+https://adventofcode.com/2015/day/17
+
+https://adventofcode.com/2015/day/17#part2
+
---
+++ 2015/17/main.py
@@ -0,0 +1,67 @@
+'''
+https://adventofcode.com/2015/day/17
+https://adventofcode.com/2015/day/17#part2
+'''
+
+
+from pathlib import Path
+import logging
+
+logger = logging.getLogger(__name__)
+
+def solution1(quiz_input):
+    print(f'{quiz_input = !r}')
+    return None
+
+def solution2(quiz_input):
+    print(f'{quiz_input = !r}')
+    return None
+
+def load_input(filename):
+    with open(filename, 'r') as fp:
+        return fp.read()
+
+def main(
+            path:Path|str,
+            input_file1:str,
+            input_file2:str,
+    ):
+    logger.info('entering module.main')
+    for id, (input_file, solution) in enumerate(zip(
+            (input_file1, input_file2), 
+            (solution1, solution2)
+        ), start=1):
+        input_path = path / input_file
+        logger.info(f'running: file {input_path}')
+        input_value = load_input(input_path)
+        result = solution(input_value)
+        logger.info(f'solution {id}: {result = }')
+    logger.info('exiting module.main')
+
+class SolutionNotFoundError(Exception): pass
+def test(
+            path:Path|str,
+            part,
+            test_file:str=None,
+            expected=None
+    ):
+    logger.info('entering module.test')
+    test_path = path / test_file
+
+    logger.info(f'testing: file {test_path}')
+    logger.info(f'testing: {expected = }')
+    
+    test_value = load_input(test_path)
+    try:
+        solution_name = f'solution{part}'
+        solution = globals()[solution_name]
+    except KeyError as ke:
+        raise SolutionNotFoundError(f'function {solution_name} not found')
+    
+    result = solution(test_value, test=True)
+    result = str(result)
+    assert result == expected, f'TEST FAILED: {result=!r} != {expected!r}'
+    logger.info('exiting module.test')
+
+if __name__ == '__main__':
+    print('please run root main.py')
