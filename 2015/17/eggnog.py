e87a6b04c48e112ce788a74d27ac797a6bb2b2b5
atrox3d <atrox3d@gmail.com>
2024-06-08 22:04:05 +0200

    add docstrings

--- 2015/17/eggnog.py
+++ 2015/17/eggnog.py
@@ -3,7 +3,8 @@ import itertools
 example = 25, [20, 15, 10, 5, 5]
 target, numbers = example
 
-def subset_sum(numbers: list, target: int) -> list[list]:
+def subset_sum(target: int, numbers: list) -> list[list]:
+    '''return every combinations with sum == target'''
     combos = []
     for i in range(len(numbers)):
         for combo in itertools.combinations(numbers, i):
@@ -12,10 +13,11 @@ def subset_sum(numbers: list, target: int) -> list[list]:
     return combos
 
 def test_combinations(data, max_len=None):
+    '''test ipertools.combinations'''
     for r in range(max_len if max_len is not None else len(data)):
         print(f'{r=}')
         for combo in itertools.combinations(data, r=r):
             print(combo)
         print()
 
-print(subset_sum(numbers, target))
+print(subset_sum(target, numbers))
