f01c6502bffd4726f0966c34a385aadc0f321b31
atrox3d <atrox3d@gmail.com>
2024-06-22 21:27:31 +0200

    use offset var

--- 2015/20/temp.py
+++ 2015/20/temp.py
@@ -1,19 +1,19 @@
 target = 33_100_000
 max_houses = 1_000_000
 
-def get_house(target, max_houses):
+def get_house(target, max_houses, offset=1):
     presents = [0] * max_houses             # create arrays of 0
     for house in range(1, max_houses+1):    # loop over every house
-        tmp = house - 1                     # adjust index to zero based?
+        tmp = house - offset                # adjust index to zero based?
 
         while tmp < max_houses:             # 
 
             presents[tmp] += 10 * house
 
             tmp += house
-        if presents[house - 1] >= target:
+        if presents[house - offset] >= target:
             print(f"Part 1: {house:_}")
-            return house, presents[house-1]
+            return house, presents[house-offset]
 
 def get_presents_per_house(house:int) -> int:
     return sum(filter(lambda elf: house%elf==0, range(1, house+1))) * 10
