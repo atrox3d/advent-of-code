30212a5e4cc53b7cf19bd985a729bf205903e57f
atrox3d <atrox3d@gmail.com>
2024-06-26 07:28:05 +0200

    add build stats

--- 2015/21/game.py
+++ 2015/21/game.py
@@ -2,7 +2,7 @@
 import itertools
 import string
 from typing import Generator
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
 
 from shop import Shop, ShopItem, Weapon, Armor, Ring 
@@ -13,6 +13,21 @@ class Build:
     armor:Armor
     lring:Ring
     rring:Ring
+    total_damage:int = 0
+    total_armor:int = 0
+    total_cost:int = 0
+
+    def __post_init__(self):
+        self.total_damage = sum(item.damage for item in self.items())
+        self.total_armor = sum(item.armor for item in self.items())
+        self.total_cost =  sum(item.cost for item in self.items())
+
+    def items(self) -> tuple[Weapon, Armor, Ring, Ring]:
+        return self.weapon, self.armor, self.lring, self.rring
+    
+    def get_stats(self) -> tuple[int, int, int]:
+        return self.total_damage, self.total_armor, self.total_cost
+
 
 def get_builds(shop:Shop) -> Generator[Build, None, None]:
     weapons = shop.weapons()
@@ -24,8 +39,6 @@ def get_builds(shop:Shop) -> Generator[Build, None, None]:
 
     for weapon, armor, lring, rring in itertools.product(weapons, armors, leftrings, rightrings):
         if lring is not rring:
-            # print(weapon.name, armor.name, lring.name, rring.name)
-            # nametpl = (weapon.name, armor.name, lring.name, rring.name)
             build = Build(weapon, armor, lring, rring)
             yield build
     
@@ -34,4 +47,6 @@ if __name__ == '__main__':
     shop = Shop()
 
     for build in get_builds(shop):
-        print(build.weapon.name, build.armor.name, build.lring.name, build.rring.name)
+        print(build.weapon.name, build.armor.name, build.lring.name, build.rring.name,
+               build.get_stats())
+
