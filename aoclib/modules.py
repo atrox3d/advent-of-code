80888adbeba803dee8c351ff65a448809dc6e6fc
atrox3d <atrox3d@gmail.com>
2024-11-06 06:31:29 +0100

    fix import from same path

--- aoclib/modules.py
+++ aoclib/modules.py
@@ -13,24 +13,22 @@ def load_module_from_path(
     '''dynamically loads a module from path and filename'''
 
     file_path = Path(target_path)
-    print(f'{file_path = }')
     if not file_path.exists():
         raise FileNotFoundError(file_path)
+    
+    # get just the file name without ext
     module_name = file_path.stem
-    print(f'{module_name = }')
 
+    # add the file path to sys.path, this way the loaded module
+    # can import siblings
     sys.path.insert(0, str(file_path.parent))
 
-    # logger.debug(f'loading module file: {file_path}')
     spec = importlib.util.spec_from_file_location(module_name, file_path)
-
-    # logger.debug(f'creating module fromo spec: {spec}')
     module = importlib.util.module_from_spec(spec)
 
     if cache_module:
         sys.modules[module_name] = module
 
-    # logger.debug(f'executing module: {module}')
     spec.loader.exec_module(module)
     return module
 
