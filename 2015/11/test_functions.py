107ae507b1af96a60a40490d7fabee023d92d42b
atrox3d <atrox3d@gmail.com>
2024-11-14 14:35:45 +0100

    test-increment

--- 2015/11/test_functions.py
+++ 2015/11/test_functions.py
@@ -3,13 +3,13 @@ import pytest
 try:
     from increment import increment
     from rincrement import rincrement
-    from valid import is_valid, valid_chars, forbidden
+    from valid import is_valid, valid_chars, forbidden, check_chars
     from doubles import has_doubles
     from straight import has_straight, get_straights
 except:
     from .increment import increment
     from .rincrement import rincrement
-    from .valid import is_valid, valid_chars, forbidden
+    from .valid import is_valid, valid_chars, forbidden, check_chars
     from .doubles import has_doubles
     from .straight import has_straight, get_straights
 
@@ -30,3 +30,18 @@ def test_doubles():
     assert not has_doubles('aabbaa')
     assert has_doubles('aabbccc')
     assert not has_doubles('aabbcccc')
+    assert not has_doubles('abcdefgh')
+    assert has_doubles('ghjaaabb')
+    assert has_doubles('gjaaabcc')
+    assert has_doubles('abcdffaa')
+    assert has_doubles('ghjaabcc')
+    assert has_doubles('gjaabbcd')
+
+def test_check_chars():
+    assert check_chars(valid_chars, valid_chars, forbidden)
+    assert not check_chars(forbidden, valid_chars, forbidden)
+
+def test_increment():
+    assert increment('a') == 'b'
+    assert increment('ab') == 'ac'
+    assert increment('az') == 'ba'
