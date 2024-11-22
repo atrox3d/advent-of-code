4ff527380532cfde31815d38754bdef29b3bf0f4
atrox3d <atrox3d@gmail.com>
2024-11-22 06:52:01 +0100

    test public methods

--- 2015/21/test_shop.py
+++ 2015/21/test_shop.py
@@ -30,4 +30,18 @@ def test_categories():
     categories = s.categories()
     print(categories)
     assert categories == ['Armors', 'Rings', 'Weapons']
-    
+
+def test_weapons():
+    s = shop.Shop()
+    for weapon in s.weapons():
+        assert weapon.category == 'Weapons'
+
+def test_armors():
+    s = shop.Shop()
+    for weapon in s.armors():
+        assert weapon.category == 'Armors'
+
+def test_rings():
+    s = shop.Shop()
+    for weapon in s.rings():
+        assert weapon.category == 'Rings'
